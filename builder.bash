f9de80b47903b6179b3dd10bdefd12880c79e8c4
WebbyJD <declanjwebb307@gmail.com>
2026-03-13 15:01:10 +0000

    Update builder.bash

--- builder.bash
+++ builder.bash
@@ -2,6 +2,6 @@
 
 echo "Building jar from files present in folder"
 
-javac -d build Conjuga.java
+javac -d build src/Conjuga.java
 
-jar --create --file Conjuga.jar --main-class Conjuga -C build .
+jar --create --file Conjuga.jar --main-class src.Conjuga -C build .
